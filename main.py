d3342de3d60e4e39be4b32c3d5a3dd763892007a
silasmk <silasmck@t-online.de>
2026-08-29 21:33:54 +0200

    finished asci art converter

--- main.py
+++ main.py
@@ -8,16 +8,34 @@ framecount = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
 fps = cap.get(cv.CAP_PROP_FPS)
 while True:
     ret, frame = cap.read()
+    if not ret:
+        cap.release()
+        cap = cv.VideoCapture("leopard.mp4")
+        ret, frame = cap.read()
     time.sleep(1/fps)
+
     bw_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
     pxl_size = 10
     temp_small = cv.resize(bw_frame, (int(frame.shape[1] / pxl_size), int(frame.shape[0] / pxl_size)), interpolation=cv.INTER_LINEAR)
     pxl_frame = cv.resize(temp_small, (frame.shape[1], frame.shape[0]), interpolation=cv.INTER_NEAREST)
-    inv_frame = pxl_frame.copy()
-    inv_frame[:,:] = 255-pxl_frame[:,:]
-    #txt_frame = cv.putText(pxl_frame, "Test", (10,10), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 2)
-    cv.imshow("Frame", inv_frame)
+
+    black_frame = pxl_frame.copy()
+    black_frame[:,:] = 0
+
+    signs = "X.-_:,'~;^!=](r+f/)j*c?<>l1Lz7vi|st[nTJuxYIC}Fo{hEPy235Zeba4UVSkw$GXqAdOpm&D9H6QRKg8#0BWN@M"
+    l = len(signs)
+
+    for x in range(0, temp_small.shape[0]):
+        for y in range(0, temp_small.shape[1]):
+            n = temp_small[x,y]
+            n = n / 255 * l
+            n = int(l - n)
+            if n > 1:
+                black_frame = cv.putText(black_frame, signs[n], (y*pxl_size, x*pxl_size), cv.FONT_HERSHEY_SIMPLEX, 0.5, (int(temp_small[x,y])/255 * 5) ** 3 * 5, 1)
+    cv.imshow("Frame", black_frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
+    
 cv.destroyAllWindows()
 cap.release()
